2a06f46cf68b04d1907300d1db8958b51f44eec3
SooHwan <newpoo@naver.com>
2022-09-05 19:06:09 +0900

    feat: 달력 날짜 추가

--- app/src/main/java/org/tech/town/logtest/MainActivity.kt
+++ app/src/main/java/org/tech/town/logtest/MainActivity.kt
@@ -3,9 +3,12 @@ package org.tech.town.logtest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.jakewharton.threetenabp.AndroidThreeTen
 import org.tech.town.logtest.databinding.ActivityMainBinding
 import org.threeten.bp.LocalDate
+import org.threeten.bp.YearMonth
 import org.threeten.bp.format.DateTimeFormatter
 
 
@@ -19,10 +22,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         //binding 초기화
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        AndroidThreeTen.init(this)
 
         //현재 날짜
         selectedDate  = LocalDate.now()
@@ -48,6 +50,21 @@ class MainActivity : AppCompatActivity() {
     private fun setMonthView() {
         //년월 텍스트뷰 셋팅
         binding.monthYearText.text = monthYearFromDate(selectedDate)
+
+        //날짜 생성해서 리스트에 담기
+        val dayList = dayInMonthArray(selectedDate)
+
+        //어댑터 초기화
+        val adapter = CalendarAdapter(dayList)
+
+        //레이아웃 설정(열 7개)
+        var manager: RecyclerView.LayoutManager = GridLayoutManager(applicationContext, 7)
+
+        //레이아웃 적용
+        binding.recyclerView.layoutManager = manager
+
+        //어댑터 적용
+        binding.recyclerView.adapter = adapter
     }
 
     //날짜 타입 설정
@@ -57,5 +74,33 @@ class MainActivity : AppCompatActivity() {
 
         // 받아온 날짜를 해당 포맷으로 변경
         return date.format(formatter)
+    }
+
+
+    //날짜 생성
+    private fun dayInMonthArray(date: LocalDate): ArrayList<String>{
+
+        var dayList = ArrayList<String>()
+
+        var yearMonth = YearMonth.from(date)
+
+        //해당 월 마지막 날짜 가져오기(예: 28, 30, 31)
+        var lastDay = yearMonth.lengthOfMonth()
+
+        //해당 월의 첫 번째 날 가져오기(예: 4월 1일)
+        var firstDay = selectedDate.withDayOfMonth(1)
+
+        //첫 번째날 요일 가져오기(월:1, 일: 7)
+        var dayOfWeek = firstDay.dayOfWeek.value
+
+        for(i in 1..41){
+            if(i <= dayOfWeek || i > (lastDay + dayOfWeek)){
+                dayList.add("")
+            }else{
+                dayList.add((i - dayOfWeek).toString())
+            }
+        }
+
+        return dayList
     }
 }
