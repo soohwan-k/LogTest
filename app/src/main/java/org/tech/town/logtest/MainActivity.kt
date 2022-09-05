746f0db80b94546ce1ede9b0d40cdb28aa1b15ad
SooHwan <newpoo@naver.com>
2022-09-05 20:53:31 +0900

    feat: 오늘 날짜 배경 색상 변경

--- app/src/main/java/org/tech/town/logtest/MainActivity.kt
+++ app/src/main/java/org/tech/town/logtest/MainActivity.kt
@@ -27,8 +27,6 @@ class MainActivity : AppCompatActivity(){
         //binding 초기화
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        //현재 날짜
-        selectedDate  = LocalDate.now()
 
         //화면 설정
         setMonthView()
@@ -36,13 +34,13 @@ class MainActivity : AppCompatActivity(){
         //이전달 버튼 이벤트
         binding.preBtn.setOnClickListener {
             //현재 월 -1 변수에 담기
-            selectedDate = selectedDate.minusMonths(1)
+            CalendarUtil.selectedDate = CalendarUtil.selectedDate.minusMonths(1)
             setMonthView()
         }
 
         //다음달 버튼 이벤트
         binding.nextBtn.setOnClickListener {
-            selectedDate = selectedDate.plusMonths(1)
+            CalendarUtil.selectedDate = CalendarUtil.selectedDate.plusMonths(1)
             setMonthView()
         }
     }
@@ -50,10 +48,10 @@ class MainActivity : AppCompatActivity(){
     //날짜 화면에 보여주기
     private fun setMonthView() {
         //년월 텍스트뷰 셋팅
-        binding.monthYearText.text = monthYearFromDate(selectedDate)
+        binding.monthYearText.text = monthYearFromDate(CalendarUtil.selectedDate)
 
         //날짜 생성해서 리스트에 담기
-        val dayList = dayInMonthArray(selectedDate)
+        val dayList = dayInMonthArray(CalendarUtil.selectedDate)
 
         //어댑터 초기화
         val adapter = CalendarAdapter(dayList)
@@ -94,7 +92,7 @@ class MainActivity : AppCompatActivity(){
         var lastDay = yearMonth.lengthOfMonth()
 
         //해당 월의 첫 번째 날 가져오기(예: 4월 1일)
-        var firstDay = selectedDate.withDayOfMonth(1)
+        var firstDay = CalendarUtil.selectedDate.withDayOfMonth(1)
 
         //첫 번째날 요일 가져오기(월:1, 일: 7)
         var dayOfWeek = firstDay.dayOfWeek.value
@@ -103,7 +101,7 @@ class MainActivity : AppCompatActivity(){
             if(i <= dayOfWeek || i > (lastDay + dayOfWeek)){
                 dayList.add(null)
             }else{
-                dayList.add(LocalDate.of(selectedDate.year, selectedDate.monthValue, i - dayOfWeek))
+                dayList.add(LocalDate.of(CalendarUtil.selectedDate.year, CalendarUtil.selectedDate.monthValue, i - dayOfWeek))
             }
         }
 
